d7e137c6865214b7e087b5604cb8aea6af7b7118
sdreyes <sheshechan90@gmail.com>
2019-04-01 13:33:53 -0400

    Create a Jumbotron that contains the game instructions and a tagline

---
+++ src/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Wrapper from "../Wrapper";
+
+function Title() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>Michael Scott Clicky Game</h1>
+            <h4>
+            Click on an image to earn points, but don't click on any more than once!
+            </h4>
+            <small>"I hate so much about the things that this game chooses to be"</small>
+        </div>
+    )
+}
+
+export default Title;
