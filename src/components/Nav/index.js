0e286c622a2fe82b8821719d1b08a96d2f22d5ac
sdreyes <sheshechan90@gmail.com>
2019-04-01 13:39:34 -0400

    Only display the score and top score in the navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,12 +4,6 @@ function Nav(props) {
     return (
         <nav className="navbar sticky-top navbar-dark bg-primary d-flex justify-content-around">
             <h3>
-                Clicky Game
-            </h3>
-            <h3>
-                
-            </h3>
-            <h3>
                 Score: {props.score} | Top Score: {props.topScore}
             </h3>
         </nav>
