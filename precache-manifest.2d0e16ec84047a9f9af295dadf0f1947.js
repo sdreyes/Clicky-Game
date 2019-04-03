05503e26f799c4cf541ee417df6d1797e549a841
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-04-02 20:19:45 -0400

    Updates

---
+++ precache-manifest.2d0e16ec84047a9f9af295dadf0f1947.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "dc0ec708fb707b67292d",
+    "url": "/clicky-game/static/js/main.abcfcf75.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f0194248d83c8ddeae369f25f6eb99d2",
+    "url": "/clicky-game/index.html"
+  }
+];
