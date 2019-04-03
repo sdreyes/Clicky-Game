083247bc7dca636f1762d8eb6d8ecc7e87249b23
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2019-04-02 21:34:08 -0400

    On large viewports, shrink the width of the container

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Row(props) {
-    return <div className="row">{props.children}</div>
+    return (
+        <div className="row justify-content-center">
+            <div className="col-lg-9 col-sm-12">
+                <div className="row">{props.children}</div>
+            </div>
+        </div>
+    )
 }
 
 export default Row;
