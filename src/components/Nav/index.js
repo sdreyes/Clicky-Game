7015148e47728d058382070d9d00505116893a3e
sdreyes <sheshechan90@gmail.com>
2019-04-03 15:15:10 -0400

    Change the color of the nav bar from blue to teal

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Nav(props) {
     return (
-        <nav className="navbar sticky-top navbar-dark bg-primary d-flex justify-content-around">
+        <nav className="navbar sticky-top navbar-dark bg-info d-flex justify-content-around">
             <h3>
                 Score: {props.score} | Top Score: {props.topScore}
             </h3>
